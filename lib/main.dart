56c114731b49a8d065ce945007bd4a7d7ca7db88
aathil-Mr-ITGuy <ibnusafina@gmail.com>
2021-06-25 15:48:43 +0530

    Flutter Button Bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,59 +10,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  double _volume = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Volume Controller'),
+        title: Text("Button Bar"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.remove),
-                  color: Colors.blue,
-                  iconSize: 50,
-                  onPressed: () {
-                    setState(() {
-                      // if (_volume != 0.0)
-                      while (_volume >= 0.1) {
-                        _volume -= 10.0;
-                      }
-                    });
-                  },
-                ),
-                IconButton(
-                  icon: Icon(Icons.volume_up),
-                  color: Colors.black,
-                  iconSize: 50,
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  color: Colors.blue,
-                  iconSize: 50,
-                  onPressed: () {
-                    setState(() {
-                      _volume += 10.0;
-                    });
-                  },
-                ),
-              ],
-            ),
-          ),
-          Center(
-            child: Text(
-              'Volume is $_volume',
-              style: TextStyle(fontSize: 24, color: Colors.red),
-            ),
-          )
-        ],
+      body: Container(
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  print("Home");
+                }),
+            IconButton(
+                icon: Icon(Icons.inbox),
+                onPressed: () {
+                  print("Inbox");
+                }),
+            IconButton(
+                icon: Icon(Icons.mail),
+                onPressed: () {
+                  print("Mail");
+                }),
+            IconButton(
+                icon: Icon(Icons.local_offer),
+                onPressed: () {
+                  print("Promotion");
+                })
+          ],
+        ),
       ),
     );
   }
