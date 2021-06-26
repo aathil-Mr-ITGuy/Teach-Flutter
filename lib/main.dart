4c014b54dab7a7c3a1e60d4b9f2ca3e434c819e5
aathil-Mr-ITGuy <ibnusafina@gmail.com>
2021-06-26 10:45:06 +0530

    Single Child Scroll View for Rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(home: new MyApp()));
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,39 +15,37 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Button Bar"),
+      appBar: AppBar(
+        title: Text("Row Scroll"),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Container(height: 100, width: 150, color: Colors.red),
+            SizedBox(
+              width: 10,
+            ),
+            Container(height: 100, width: 150, color: Colors.yellow),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 100,
+              width: 150,
+              color: Colors.orange,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 100,
+              width: 150,
+              color: Colors.purple,
+            )
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 250,
-                color: Colors.red,
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 250,
-                color: Colors.yellow,
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 250,
-                color: Colors.orange,
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 250,
-                color: Colors.yellowAccent,
-              )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
